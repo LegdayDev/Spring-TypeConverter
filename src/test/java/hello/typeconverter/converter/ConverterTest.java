1d9a4e641be61d00f529aae5c07383ee5dba3d23
Legday <chlwodud0327@naver.com>
2023-10-27 15:46:01 +0900

    Feat: 스프링등록 없이 Converter 인터페이스 구현해보기
    Integer -> String , String -> Integer 컨버터를 구현해서 테스트 완료
    IpPort(객체) -> String , String -> IpPort 컨버터 구현해서 테스트 완료
    
    스프링 등록 없이 직접 객체를 불러와서 사용해서 긴가민가 하다

---
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,45 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConverterTest {
+
+    @Test
+    @DisplayName("String -> Integer 컨버트")
+    void stringToInteger(){
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("Integer -> String 컨버트")
+    void integerToString(){
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    @DisplayName("String -> IpPort 컨버트")
+    void stringToIpPort(){
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1",8080));
+    }
+
+    @Test
+    @DisplayName("IpPort -> String 컨버트")
+    void ipPortToString(){
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(ipPort);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+}
